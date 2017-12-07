f6c0734cf45ec87fe919ba12126c3a5b6a934398
OptionRV <adrien.jacquin@eleves.ec-nantes.fr>
2017-12-07 10:25:53 +0100

    Add deplacement method

--- MEDEV_TP3/dames.cpp
+++ MEDEV_TP3/dames.cpp
@@ -34,12 +34,31 @@ Dames::Dames() {
 
 void Dames::disp() const {
 	// Affiche le plateau de jeu
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
-			cout << plateau[i][j] << " ";
+	cout << "=====================" << endl << "  ";
+	for (int i=-1; i<N; i++) {
+		for (int j=-1; j<N; j++) {
+			if (i==-1 && j>-1) {
+				cout << j << " ";
+			}
+			if (j==-1 && i>-1) {
+				cout << i << " ";
+			}
+			if (i>-1 && j>-1) {
+				cout << plateau[i][j] << " ";
+			}
 		}
 		cout << endl;
 	}
+	cout << "=====================" << endl;
+}
+
+coord fillCoord(int x, int y, int i) {
+	// Fill a coord struct with the three arguments and return it
+	coord move;
+	move.x = x;
+	move.y = y;
+	move.index = i;
+	return move;
 }
 
 vector<coord> Dames::deplacements_possibles() {
@@ -48,42 +67,55 @@ vector<coord> Dames::deplacements_possibles() {
 	// (on associe à chaque coord l'index du pion concerné)
 	vector<coord> deplacements;
 	for (int i=0; i<pions.size(); i++) { // on parcourt tous les pions
-		coord move;
-		move.x = pions[i].getX();
-		move.y = pions[i].getY();
-		move.index = i;
+		coord move = fillCoord(pions[i].getX(), pions[i].getY(), i);
 		if (move.x-1>=0 && move.y-1>=0 && plateau[move.x-1][move.y-1]==0) {
 			// si la case en haut à gauche est libre
-			coord newMove;
-			newMove.x = move.x-1;
-			newMove.y = move.y-1;
-			newMove.index = move.index;
+			coord newMove = fillCoord(move.x-1, move.y-1, move.index);
 			deplacements.push_back(newMove);
 		}
 		if (move.x-1>=0 && move.y+1<N && plateau[move.x-1][move.y+1]==0) {
 			// si la case en bas à gauche est libre
-			coord newMove;
-			newMove.x = move.x-1;
-			newMove.y = move.y+1;
-			newMove.index = move.index;
+			coord newMove = fillCoord(move.x-1, move.y+1, move.index);
 			deplacements.push_back(newMove);
 		}
 		if (move.x+1<N && move.y-1>=0 && plateau[move.x+1][move.y-1]==0) {
 			// si la case en haut à droite est libre
-			coord newMove;
-			newMove.x = move.x+1;
-			newMove.y = move.y-1;
-			newMove.index = move.index;
+			coord newMove = fillCoord(move.x+1, move.y-1, move.index);
 			deplacements.push_back(newMove);
 		}
 		if (move.x+1<N && move.y+1<N && plateau[move.x+1][move.y+1]==0) {
 			// si la case en bas à droite est libre
-			coord newMove;
-			newMove.x = move.x+1;
-			newMove.y = move.y+1;
-			newMove.index = move.index;
+			coord newMove = fillCoord(move.x+1, move.y+1, move.index);
 			deplacements.push_back(newMove);
 		}
 	}
 	return deplacements;
+}
+
+bool Dames::deplacement(int x, int y, int u, int v) {
+	// Déplacement du pion (x,y) en (u,v)
+	// Renvoi true si le déplacement a été possible, false sinon
+	int pionIndex = -1;
+	vector<coord> deplacements = deplacements_possibles(); // liste des déplacements possibles
+	for (int i=0; i<pions.size(); i++) {
+		// on recherche l'index du pion qu'on veut déplacer
+		if (pions[i].getX() == x && pions[i].getY() == y) {
+			pionIndex = i; // index trouvé !
+		}
+	}
+	if (pionIndex == -1) {
+		return false; // on a pas trouvé d'index pour ce pion
+	}
+	for (int i=0; i<deplacements.size(); i++) {
+		// on recherche si le deplacement est possible
+		if (deplacements[i].index == pionIndex && deplacements[i].x == u && deplacements[i].y == v) {
+			// deplacement possible, on l'effectue sur le plateau :
+			plateau[x][y] = 0;
+			plateau[u][v] = 1;
+			// puis on change les coordonnées du pion dans la liste des pions :
+			pions[pionIndex].setPosition(u,v);
+			return true;
+		}
+	}
+	return false; // on n'a pas peu effectuer le déplacement demandé
 }
--- MEDEV_TP3/main.cpp
+++ MEDEV_TP3/main.cpp
@@ -8,5 +8,8 @@ int main() {
 	Dames jeu = Dames(); // initialisation du jeu et du plateau
 	vector<coord> deplacements = jeu.deplacements_possibles();
 	jeu.disp();
+	bool b = jeu.deplacement(0,3,1,4); // déplace (0,3) en (1,4)
+	cout << "deplacement effectue : " << b << endl;
+	jeu.disp();
 	return 0;
 }
